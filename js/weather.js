a5860e7bd5e2c9ee4d472f49a0f8ab0f73f6da95
jc8670 <jc8670@gmail.com>
2022-08-05 12:58:33 -0700

    finalize

--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,23 @@
 navigator.geolocation.getCurrentPosition(onGeoSucess,onGeoFailure);
 
+const API_KEY = "dd892916e8b17ee80c62e0b5e61debde";
 
-function onGeoSucess(){
-console.log(navigator);
-console.log("geo success");
+function onGeoSucess(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    fetch(URL)
+        .then((response) => response.json())
+        .then((data)=> {
+        const name = data.name;
+        const weather = data.weather[0].main;
+        });
 }
 
 
 
 
 function onGeoFailure(){
-    console.log("geo failure");
-
+    alert("Can't find you.")
 }
